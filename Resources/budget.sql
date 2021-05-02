63aa45616aa6dd38b9a7a62ddb5997ba949c2ca1
hbarwick <74908294+hbarwick@users.noreply.github.com>
2021-05-02 17:38:37 +0100

    Updated sql db backup

--- Resources/budget.sql
+++ Resources/budget.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 01, 2021 at 09:13 AM
+-- Generation Time: May 02, 2021 at 06:38 PM
 -- Server version: 10.4.18-MariaDB
--- PHP Version: 7.3.27
+-- PHP Version: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `payments` (
   `UID` int(6) NOT NULL,
+  `user` varchar(30) NOT NULL,
   `date` date DEFAULT NULL,
   `value` float DEFAULT NULL,
   `category` varchar(30) DEFAULT NULL,
@@ -39,10 +40,37 @@ CREATE TABLE `payments` (
 -- Dumping data for table `payments`
 --
 
-INSERT INTO `payments` (`UID`, `date`, `value`, `category`, `extra_details`) VALUES
-(1, '0000-00-00', 30.32, 'Booze', 'Lots of Booze'),
-(2, '2021-04-30', 30.32, 'Booze', 'Lots of Booze'),
-(3, '2021-04-30', 20.32, 'Catfood', 'None');
+INSERT INTO `payments` (`UID`, `user`, `date`, `value`, `category`, `extra_details`) VALUES
+(2, 'hal', '2021-04-30', 30.32, 'Booze', 'Lots of Booze'),
+(3, 'q', '2021-05-01', 20.32, 'Catfood', 'None'),
+(4, 'hal', '2021-05-01', 23.5, 'Tea', '5 cups of tea'),
+(5, 'hal', '2021-05-01', 46.23, 'Cheese', ''),
+(6, 'hal', '2021-05-02', 35.2, 'Chairs', 'some dining chairs'),
+(7, 'q', '2021-05-02', 56.2, 'Jumpers', '3 wolly jumpers'),
+(8, 'hal', '2021-05-02', 2, '2', '2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(30) NOT NULL,
+  `first_name` varchar(30) DEFAULT NULL,
+  `last_name` varchar(30) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `password` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `first_name`, `last_name`, `email`, `password`) VALUES
+('hal', 'hal', 'barwick', 'hal@b', 'hal'),
+('q', 'q', 'q', 'q', 'q'),
+('w', 'w', 'w', 'w', 'w');
 
 --
 -- Indexes for dumped tables
@@ -55,6 +83,12 @@ ALTER TABLE `payments`
   ADD PRIMARY KEY (`UID`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -62,7 +96,7 @@ ALTER TABLE `payments`
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `UID` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `UID` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
