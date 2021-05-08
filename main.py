0e64aaba52eb10cf83c6a6c6f9537065968f074a
hbarwick <74908294+hbarwick@users.noreply.github.com>
2021-05-08 10:38:30 +0100

    Finished Yes/No popup for deleting payment on paymentscreen and split out class to paymentscreen.py

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.properties import StringProperty
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
-from dbclasses import DataBaseObject, Payment, Income
+from dbclasses import DataBaseObject, Income
 from globalmethods import popup_message
-from newuserscreen import NewUserScreen
 from datetime import date as dt
 
 Window.size = (480, 800)
@@ -81,67 +79,21 @@ class MainMenu(Screen):
         self.manager.current = 'income_screen'
 
 
-class PaymentScreen(Screen):
-    """Kivy Screen to add one off payments"""
+class YesNoPopup(Popup):
+    __events__ = ('on_yes', 'on_no')
 
-    def get_payments_from_db(self):
-        """Return the total of payments for the
-        currently logged in user for the current month."""
-        user = self.manager.current_user
-        month = dt.today().month
-        db = DataBaseObject()
-        paymentquery = db.fetch_data(
-            f"""SELECT value, category, extra_details FROM payments 
-                WHERE user = '{user}'
-                AND month(date) = '{month}'
-                """)
-        db.close_database_connection()
-        return paymentquery
+    message = StringProperty('')
 
-    def get_payments(self):
-        payments = self.get_payments_from_db()
-        payment_string = ""
-        total_payments = 0
-        for i in payments:
-            payment_string += f"£{i[0]} - {i[1]} - '{i[2]}'\n"
-            total_payments += i[0]
-        self.manager.current_screen.ids.month_payments.text = payment_string
+    def __init__(self, **kwargs) -> None:
+        super(YesNoPopup, self).__init__(**kwargs)
+        self.auto_dismiss = False
 
-    def submit_payment(self):
-        """Submits a payment to the database. Takes the value,
-        category and Extra details from the Kivy input fields,
-        date from today's date, and username from current user
-         logged in"""
-        user = self.manager.current_user
-        date = dt.today().isoformat()
-        value = self.manager.current_screen.ids.value.text
-        category = self.manager.current_screen.ids.category.text
-        extra_details = self.manager.current_screen.ids.extra_details.text
+    def on_yes(self):
+        pass
 
-        payment = Payment(user, date, value, category, extra_details)
-        payment.update_database()
-        popup_message("Success", "New Payment Added"
-                                 f"\n {category}\n£{value}\n{extra_details}")
-        self.manager.current = 'main_menu'
+    def on_no(self):
+        pass
 
-    def cancel(self):
-        self.manager.current = 'main_menu'
-
-    def delete_button(self):
-        show = Popups()
-        popupWindow = Popup(title="Are you sure?", content=show,
-                            size_hint=(None, None), size=(200, 200))
-        self.manager.Popups.ids.yes_button.on_press = self.cancel()
-        popupWindow.open()
-
-    def delete_last_payment(self):
-        db = DataBaseObject()
-        db.run_database_command(
-            "DELETE FROM payments order by UID DESC limit 1")
-
-
-class Popups(FloatLayout):
-    pass
 
 class BillScreen(Screen):
     pass
