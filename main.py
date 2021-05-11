3cb690f82fcbe0a3f3b254911a20b4ea76790827
hbarwick <74908294+hbarwick@users.noreply.github.com>
2021-05-11 15:28:33 +0100

    Amended file structure

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import Screen
 from dbclasses import DataBaseObject, Income, Bill, Payment
 from globalmethods import popup_message
-from newuserscreen import NewUserScreen
-
 
 Window.size = (480, 800)
 
@@ -60,6 +58,57 @@ class LogonScreen(Screen):
             return False
         else:
             return True
+
+
+class NewUserScreen(Screen):
+
+    """Kivy Screen to create new user and add to
+    the MySQL database"""
+
+    def new_user(self):
+        username = self.manager.current_screen.ids.username.text
+        first_name = self.manager.current_screen.ids.first_name.text
+        last_name = self.manager.current_screen.ids.last_name.text
+        email = self.manager.current_screen.ids.email.text
+        password = self.manager.current_screen.ids.password.text
+        confirm_password = self.manager.current_screen.ids.confirm_password.text
+        userdetails = (username, first_name, last_name, email, password, confirm_password)
+        return userdetails
+
+    # TODO implement password hashing
+
+    def usercheck(self):
+        userdetails = self.new_user()
+        if userdetails[4] != userdetails[5]:
+            popup_message("Warning", "Passwords do not match")
+            return
+        elif "" in userdetails:
+            popup_message("Warning", "Not all fields populated.")
+            return
+        else:
+            user = User(userdetails[0],
+                        userdetails[1],
+                        userdetails[2],
+                        userdetails[3],
+                        userdetails[4])
+            if user.check_duplicate():
+                popup_message("Warning", "Username is taken")
+                return
+            user.update_database()
+            popup_message("Success!", f"New user '{user.username}' created.")
+            self.reset_input_fields()
+            self.manager.current = 'logon_screen'
+
+    def reset_input_fields(self):
+        self.manager.current_screen.ids.username.text = ""
+        self.manager.current_screen.ids.first_name.text = ""
+        self.manager.current_screen.ids.last_name.text = ""
+        self.manager.current_screen.ids.email.text = ""
+        self.manager.current_screen.ids.password.text = ""
+        self.manager.current_screen.ids.confirm_password.text = ""
+
+    def cancel(self):
+        self.manager.current = 'logon_screen'
 
 
 class MainMenu(Screen):
